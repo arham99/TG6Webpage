0b9c198768a1870685ac59976f78681d4015f0d0
arham zakki edelo <azedelo1999@gmail.com>
2021-08-14 10:58:44 +0700

    second refrehs

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,6 +1,6 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: -6.207690, lng: 106.985270};
+    var myLatLng = {lat: -7.28763, lng: 112.80087};
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
         zoom: 17,
